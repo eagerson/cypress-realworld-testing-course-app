8a62222c8a4238665f5936fd1d6c3174db14a61f
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-09-03 18:00:24 +0545

    feat: add test to newsletter subscrition for invalid email

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -10,4 +10,12 @@ describe("Newsletter Subscribe Form", () => {
     cy.getByData("submit-button").click() // click on submit button
     cy.getByData("success-message").should("exist").contains(email) // check if success message is shown for the provided email
   })
+
+  it("does NOT allow an invalid email address", () => {
+    const email = "tom" // invalid email
+
+    cy.getByData("email-input").type(email) // grab the email input box and type email
+    cy.getByData("submit-button").click() // click on submit button
+    cy.getByData("success-message").should("not.exist") // check if success message is not shown for the provided email
+  })
 })
