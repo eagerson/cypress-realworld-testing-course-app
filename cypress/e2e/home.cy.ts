4bf5b075f5f38af893526467697be2a53b333d3d
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-09-03 14:39:53 +0545

    perf: move code to visit site from all tests to beforeEach of the test suite where the code will be executes before running any tests on that test suite

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,12 +1,15 @@
 describe("home page", () => {
-  it("the h1 contains the correct text", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000")
+  })
+
+  it("the h1 contains the correct text", () => {
     cy.get("[data-test='hero-heading']").contains(
       "Testing Next.js Applications with Cypress"
     )
   })
+
   it.only("the features on the homepage are correct", () => {
-    cy.visit("http://localhost:3000")
     //cy.get("dt").eq(0).contains(/4 courses/i) // making case insensitive
     cy.get("dt").eq(0).contains("4 Courses")
     cy.get("dt").eq(1).contains("25+ Lessons")
