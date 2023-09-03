b616a9affcbdc18d20378e20d265364c08028680
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-09-03 14:25:21 +0545

    feat: update new test to check if first <dt> element contains content "4 Courses"

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -7,5 +7,7 @@ describe("home page", () => {
   })
   it.only("the features on the homepage are correct", () => {
     cy.visit("http://localhost:3000")
+    //cy.get("dt").eq(0).contains(/4 courses/i) // making case insensitive
+    cy.get("dt").eq(0).contains("4 Courses")
   })
 })
