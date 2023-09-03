7de45712e363bb1c33804a374365198e0e73d146
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-09-03 14:16:14 +0545

    feat: add simple dummy test in home page test suite

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -5,4 +5,7 @@ describe("home page", () => {
       "Testing Next.js Applications with Cypress"
     )
   })
+  it("the features on the homepage are correct", () => {
+    cy.visit("http://localhost:3000")
+  })
 })
